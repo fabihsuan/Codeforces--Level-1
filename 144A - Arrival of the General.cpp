006d0cad938ac34e65cd3fc7011f14ad94b373af
fabihsuan <fabihsuan@gmail.com>
2020-09-04 16:49:38 +0800

    Create 144A - Arrival of the General.cpp

---
+++ 144A - Arrival of the General.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int x;
+  vector<int> a;
+  for (int i = 0; i < n; i++) {
+    cin >> x;
+    a.push_back(x);
+  }
+
+  auto p = minmax_element(a.rbegin(), a.rend());
+  int yy = p.first - a.rbegin();   // min
+  int zz = p.second - a.rbegin();  // max
+
+  int ans = yy + n - 1 - zz;
+  if (yy > zz) {
+    ans--;
+  }
+  cout << ans << endl;
+  return 0;
+}
