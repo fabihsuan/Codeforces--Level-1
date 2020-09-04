755c7f1bbd80b7c556ec7cf08505cdc3154177f6
fabihsuan <fabihsuan@gmail.com>
2020-09-04 16:59:23 +0800

    Create 112A - Petya and Strings.cpp

---
+++ 112A - Petya and Strings.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string str1, str2;
+  getline(cin, str1);
+  getline(cin, str2);
+  bool equal = false;
+  bool larger = false;
+  int l = str1.length();
+
+  transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
+  transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+
+  for (int i = 0; i < l; i++) {
+       if (int(str1[i]) > int(str2[i])) {
+      cout << 1 << endl;
+      return 0;
+    } else if (int(str1[i]) < int(str2[i])) {
+      cout << -1 << endl;
+      return 0;
+    }
+  }
+  cout << 0 << endl;
+  return 0;
+}
