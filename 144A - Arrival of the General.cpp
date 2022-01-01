0228af4c6bf7fde3bc212dd1aa329366731e15ef
PinHsuan <fabihsuan@gmail.com>
2022-01-01 15:42:49 +0100

    Update 144A - Arrival of the General.cpp
    add another version

--- 144A - Arrival of the General.cpp
+++ 144A - Arrival of the General.cpp
@@ -25,3 +25,39 @@ int main() {
   cout << ans << endl;
   return 0;
 }
+
+//version 2
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int x;
+  vector<int> a;
+  for (int i = 0; i < n; i++) {
+    cin >> x;
+    a.push_back(x);
+  }
+
+  auto findmax = max_element(a.begin(), a.end());
+  int max = findmax - a.begin();
+
+  auto findmin = min_element(a.rbegin(), a.rend());
+  int min = findmin - a.rbegin();
+
+  int ans = max + min;
+
+  if (ans >= n) {
+    ans--;
+  }
+
+  cout << ans << endl;
+
+  return 0;
+}
