8770043eccfd80155b73c1cfb3c2896a00012da1
PinHsuan <fabihsuan@gmail.com>
2022-01-01 19:50:52 +0100

    Update 148A - Insomnia cure.cpp
    edit

--- 148A - Insomnia cure.cpp
+++ 148A - Insomnia cure.cpp
@@ -26,3 +26,34 @@ int main() {
   cout << count << endl;
   return 0;
 }
+
+//2022.01.01
+//edit version 
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//   int a, b, c, d, e;
+//   int count = 0;
+
+//   cin >> a;
+//   cin >> b;
+//   cin >> c;
+//   cin >> d;
+//   cin >> e;
+
+//   for (int i = 1; i <= e; i++) {
+//     if (i % a == 0) {
+//       count++;
+//     } else if (i % b == 0) {
+//       count++;
+//     } else if (i % c == 0) {
+//       count++;
+//     } else if (i % d == 0) {
+//       count++;
+//     }
+//   }
+//   cout << count << endl;
+//   return 0;
+// }
