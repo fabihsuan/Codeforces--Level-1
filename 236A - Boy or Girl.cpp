4e79b5e4e8c5072f65019e96117490732bbd4779
PinHsuan <fabihsuan@gmail.com>
2022-01-05 10:38:53 +0100

    Update 236A - Boy or Girl.cpp
    map version

--- 236A - Boy or Girl.cpp
+++ 236A - Boy or Girl.cpp
@@ -15,3 +15,26 @@ int main() {
   }
   return 0;
 }
+
+
+//2022/01/04
+// #include <algorithm>
+// #include <iostream>
+// #include <map>
+// #include <string>
+// using namespace std;
+
+// int main() {
+//   string s;
+//   cin >> s;
+
+//   map<char, int> mp;
+//   for (auto c : s) ++mp[c];
+
+//   if (mp.size() % 2 == 0) {
+//     cout << "CHAT WITH HER!" << endl;
+//   } else {
+//     cout << "IGNORE HIM!" << endl;
+//   }
+//   return 0;
+// }
